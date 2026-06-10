877aa29582d6b1569e1c43f006d6605813613279
jagat1980 <jaggu_1980@yahoo.co.in>
2026-06-10 15:06:10 +0000

    secops(swarm): automatic remediation compliance patch for incident ALERT-TEST-001

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -41,10 +41,10 @@ resource "azurerm_storage_account" "vulnerable_storage" {
   account_kind             = "StorageV2"
 
   # DRIFT PROFILE: Allows nested child assets to bypass private settings
-  allow_nested_items_to_be_public = true
+  allow_nested_items_to_be_public = false
   
-  min_tls_version                 = "TLS1_2"
-  https_traffic_only_enabled      = true
+  min_tls_version            = "TLS1_2"
+  https_traffic_only_enabled = true
 
   tags = {
     ComplianceRisk = "Intentional-Drift"
@@ -53,9 +53,9 @@ resource "azurerm_storage_account" "vulnerable_storage" {
 
 # 4. Public Anonymous Access Container
 resource "azurerm_storage_container" "anonymous_container" {
-  name                  = "azure-webjobs-hosts"
-  storage_account_id    = azurerm_storage_account.vulnerable_storage.id
+  name               = "azure-webjobs-hosts"
+  storage_account_id = azurerm_storage_account.vulnerable_storage.id
   
   # DRIFT PROFILE: Exposes full listing access anonymously to the internet
-  container_access_type = "container" 
+  container_access_type = "private"
 }
