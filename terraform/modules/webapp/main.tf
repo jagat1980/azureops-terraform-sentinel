9a37425ee68e5f354513e8e4f9d82c26edddc511
jagat1980 <jaggu_1980@yahoo.co.in>
2026-07-03 09:31:34 +0000

    secops(swarm): automatic remediation compliance patch for alert CVE-2023-38325

--- terraform/modules/webapp/main.tf
+++ terraform/modules/webapp/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_web_app" "vulnerable_app" {
     scm_minimum_tls_version           = "1.2"
 
     application_stack {
-      python_version = "3.9"
+      python_version = "3.11"
     }
   }
 
